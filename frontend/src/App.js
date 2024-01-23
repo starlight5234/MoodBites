13a7b1fd90006de2a025bf1c0bde7ee2ca688e9a
Starlight5234 <starlight5234@protonmail.ch>
2024-01-24 01:27:42 +0530

    Skeleton ReactJS App
    Change-Id: I0acb6dad73b5fcabc9095615b9bc7d39fc984060
    Signed-off-by: Starlight5234 <starlight5234@protonmail.ch>

---
+++ frontend/src/App.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [responseMessage, setResponseMessage] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      const response = await fetch('http://localhost:8000/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      const data = await response.json();
+
+      setResponseMessage(data.message);
+    } catch (error) {
+      console.error('Error:', error);
+      setResponseMessage('An error occurred while processing your request.');
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1>Login App</h1>
+      <div>
+        <label>
+          Email/Phone No.:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+      </div>
+      <div>
+        <button onClick={handleLogin}>Login</button>
+      </div>
+      {responseMessage && (
+        <div>
+          <p>{responseMessage}</p>
+        </div>
+    )}
+
+    </div>
+    
+  );
+}
+
+export default App;
