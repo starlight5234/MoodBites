9ed0a86fa8c6c3615e78a28aa0bce24619044a4a
Starlight5234 <starlight5234@protonmail.ch>
2024-01-24 01:22:20 +0530

    Add User model
    Change-Id: I37ef65b8e1ccdf054e201d05970a2f27c33386ec
    Signed-off-by: Starlight5234 <starlight5234@protonmail.ch>

--- mbAPI/models.py
+++ mbAPI/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(max_length=10)
+    password = models.CharField(max_length=128)
