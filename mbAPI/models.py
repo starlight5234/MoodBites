c95370a0eeb99bc029d2f43acf57edf24b3734de
starlight5234 <starlight5234@protonmail.ch>
2024-02-11 00:45:13 +0530

    Drop User and Login model
    Change-Id: I3e742ae39e92a96ebb8278d4f9109c9e8eafd9cc
    Signed-off-by: starlight5234 <starlight5234@protonmail.ch>

--- mbAPI/models.py
+++ mbAPI/models.py
@@ -1,9 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-class User(models.Model):
-    user_id = models.AutoField(primary_key=True)
-    email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=10)
-    password = models.CharField(max_length=128)
--- mbAPI/urls.py
+++ mbAPI/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 from .views import api_root
 
-from .views import Login
-
 urlpatterns = [
     path('', api_root, name='api-root'),
-    path('login', Login.as_view(), name='login'),
 ]
