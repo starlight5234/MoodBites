cb691a027cf66aa229c0a1a034ec57ba3bfb68ff
starlight5234 <starlight5234@protonmail.ch>
2024-02-11 13:32:03 +0530

    Restaurant request Endpoint
    Change-Id: I336cd535b4a343c787cc00a2e2ccce72b428349f
    Signed-off-by: starlight5234 <starlight5234@protonmail.ch>

--- mbAPI/urls.py
+++ mbAPI/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import api_root
+from .views import api_root, reqRes
 
 urlpatterns = [
     path('', api_root, name='api-root'),
+    path('reqRes', reqRes.as_view(), name='restaurant-req'),
 ]
--- mbAPI/views.py
+++ mbAPI/views.py
@@ -7,6 +7,28 @@ from rest_framework.views import APIView
 from django.core.serializers import serialize
 from django.http import JsonResponse
 
+from mbAPI.models import RestaurantRequestModel
+from mbAPI.serializers import RestaurantRequestSerializer
+
 # Create your views here.
 def api_root(request):
     return JsonResponse({'message': 'Welcome to the API!'})
+
+class reqRes(APIView):
+    def get(self, request):
+        try:
+            # Access the "set" value from the request JSON
+            print(request.data)
+            set_value = int(request.data.get("set", 0))
+            search_limit = 9 * set_value
+            
+            # Perform a database query
+            queryset = RestaurantRequestModel.objects.all()[:search_limit]
+            
+            # Serialize the data
+            serializer = RestaurantRequestSerializer(queryset, many=True)
+            
+            # Return the serialized data
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except ValueError:
+            return Response({"error": "Invalid 'set' value in the request JSON"}, status=status.HTTP_400_BAD_REQUEST)
