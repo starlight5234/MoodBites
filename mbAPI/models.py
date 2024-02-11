26dfaa7de6773147c20fc1c58cb32f367410ba3b
starlight5234 <starlight5234@protonmail.ch>
2024-02-11 13:30:20 +0530

    Restaurant request Model and Serializer
    Change-Id: Ibb8c572358138005c8004137c2c2b7dfbe3d195f
    Signed-off-by: starlight5234 <starlight5234@protonmail.ch>

--- mbAPI/models.py
+++ mbAPI/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class RestaurantRequestModel(models.Model):
+    url = models.URLField()
+    name = models.CharField(max_length=255)
+    rest_type = models.CharField(max_length=255)
+    rate = models.DecimalField(max_digits=3, decimal_places=2)
+    cost = models.DecimalField(max_digits=6, decimal_places=2)
+    votes = models.IntegerField()
+    city = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
--- mbAPI/serializers.py
+++ mbAPI/serializers.py
@@ -1,1 +1,9 @@
 from rest_framework import serializers
+
+from mbAPI.models import RestaurantRequestModel
+
+class RestaurantRequestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = RestaurantRequestModel
+        fields = '__all__'
+        # exclude = ['id']
