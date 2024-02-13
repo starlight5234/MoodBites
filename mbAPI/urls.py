84d000e25f4c38e4c6ff451121398431c8af34ed
starlight5234 <starlight5234@protonmail.ch>
2024-02-13 12:18:48 +0530

    Store pickle file onto server

--- mbAPI/urls.py
+++ mbAPI/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import api_root, reqRes
+from .views import api_root, reqRes, PickleFileUploadView
 
 urlpatterns = [
     path('', api_root, name='api-root'),
     path('reqRes', reqRes.as_view(), name='restaurant-req'),
+    path('pickleUpload', PickleFileUploadView.as_view(), name='pickle'),
 ]
--- mbAPI/views.py
+++ mbAPI/views.py
@@ -10,6 +10,9 @@ from django.http import JsonResponse
 from mbAPI.models import Restaurant
 from mbAPI.serializers import RestaurantRequestSerializer
 
+import os
+from datetime import datetime
+
 # Create your views here.
 def api_root(request):
     return JsonResponse({'message': 'Welcome to the API!'})
@@ -33,3 +36,33 @@ class reqRes(APIView):
             return Response(serializer.data, status=status.HTTP_200_OK)
         except ValueError:
             return Response({"error": "Invalid 'index' value in the request JSON"}, status=status.HTTP_400_BAD_REQUEST)
+
+class PickleFileUploadView(APIView):
+    def post(self, request, *args, **kwargs):
+        try:
+            # Access the file using request.FILES['file']
+            uploaded_file = request.FILES['file']
+
+            # Create a "pickle" folder if it doesn't exist
+            pickle_folder = os.path.join('media', 'pickle')
+            os.makedirs(pickle_folder, exist_ok=True)
+
+            # Generate a timestamp to use in the filename
+            timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+
+            # Create the filename with the timestamp
+            filename = f"{uploaded_file.name}-{timestamp}"
+
+            # Build the full path to save the file
+            file_path = os.path.join(pickle_folder, filename)
+
+            # Save the file to the server
+            with open(file_path, 'wb') as file:
+                for chunk in uploaded_file.chunks():
+                    file.write(chunk)
+
+            # Respond with a success message
+            return Response({'message': 'File uploaded successfully.'}, status=status.HTTP_200_OK)
+
+        except Exception as e:
+            return Response({'error': f'Error uploading file: {str(e)}'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
