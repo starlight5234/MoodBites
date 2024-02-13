53f6333f002bd3e19fc7bd7d3212f17c41026215
starlight5234 <starlight5234@protonmail.ch>
2024-02-13 19:46:21 +0530

    Model: Add isRecommended field

--- mbAPI/models.py
+++ mbAPI/models.py
@@ -16,6 +16,7 @@ class Restaurant(models.Model):
     cost = models.IntegerField(default=0)
     type = models.CharField(max_length=50)
     city = models.CharField(max_length=50)
+    is_recommended = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
